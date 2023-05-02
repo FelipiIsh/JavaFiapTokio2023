3881ea2c95addf9da63b8049b024fa68a7ce56ed
logonrmlocal <logonrmlocal@PA402MICRO13.fiap.com.br>
2023-05-02 16:59:00 -0300

    aula 10 e 11

---
+++ JavaTokio2023/src/logica/aulas/aula10/Metodos.java
@@ -0,0 +1,42 @@
+package logica.aulas.aula10;
+
+import java.util.Scanner;
+
+public class Metodos {
+	
+	
+	//METODOS SEM RETORNO/PARAMETROS
+
+	static void saudacao( ) {
+		System.out.println("Bem vindo ao programa");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite seu nome: ");
+		String nome = sc.nextLine();
+		
+		System.out.println("Digite sua idade: ");
+		int idade = sc.nextInt();
+		
+		System.out.printf("Ola, %s! Voce tem %d anos", nome, idade);
+ 
+		if (idade >= 18) {
+			System.out.println("Maior de idade");
+		} else {
+			System.out.println("Menos de idade");
+		}
+	
+		sc.close();
+	
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		saudacao();
+		saudacao();
+		
+	}
+
+}
---
+++ JavaTokio2023/src/logica/aulas/aula10/MetodosComArg.java
@@ -0,0 +1,47 @@
+package logica.aulas.aula10;
+
+import java.util.Scanner;
+
+public class MetodosComArg {
+	
+	//METODOS SEM RETORNO COM ARGUMETNOS/PARAMETROS
+	
+	static void saudacaoComArgs(String nome, int idade) {
+		
+		if (idade >= 18) {
+			System.out.printf("Ola, %s! Voce tem %d anos e eh maior de idade", nome, idade);
+		} else {
+			System.out.printf("Ola, %s! Voce tem %d anos e eh menor de idade", nome, idade);
+		}
+	}
+	
+	
+	static void somar(int numA, int numB) {
+		System.out.println("Soma: " +(numA + numB));
+	}
+	
+
+	public static void main(String[] args) {
+		
+		//SOMAR 2 NUMEROS
+		somar(3, 9);
+		
+		
+		/*
+		Scanner sc = new Scanner(System.in);
+		
+
+		System.out.println("Digite seu nome: ");
+		String nomeDigitado = sc.nextLine();
+		
+		System.out.println("Digite sua idade: ");
+		int idadeDigitada = sc.nextInt();
+
+		saudacaoComArgs(nomeDigitado, idadeDigitada);
+		
+		sc.close();
+		*/
+		
+	}
+
+}
